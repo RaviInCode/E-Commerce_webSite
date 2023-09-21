7d6ebe991d19a5dc2ba7090bf79bf21778bdc174
Ravi Gupta <105039475+RaviInCode@users.noreply.github.com>
2023-09-21 17:35:31 +0530

    Add files via upload

---
+++ Build-and-Deploy-Ecommerce-Website-7bf0a6a00c79e37d97a953ed62f47197cd567ed3/abstract class Shape { abstract void dra.java
@@ -0,0 +1,1 @@
+abstract class Shape { abstract void draw(); } class Circle extends Shape { void draw() { System.out.println("Drawing a circle"); } } class Rectangle extends Shape { void draw() { System.out.println("Drawing a rectangle"); } } public class Main { public static void main(String[] args) { Shape shape1 = new Circle(); Shape shape2 = new Rectangle(); shape1
